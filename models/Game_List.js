6bc78012cd45b209df4da64df54ad71409e9e8fa
Lindsay <lshank017@gmail.com>
2023-09-11 17:59:50 -0500

    fixed server errors, now up and running

--- models/Game_List.js
+++ models/Game_List.js
@@ -4,38 +4,41 @@ const sequelize = require('../config/connection')
 
 class Game_List extends Model{}
 
-Game_List.init({
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
+Game_List.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    game: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    genre: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    difficulty: {
+      type: DataTypes.ENUM(["easy", "medium", "hard"]),
+      allowNull: true,
+    },
+    length: {
+      type: DataTypes.DECIMAL(4, 2),
+      allowNull: true,
+    },
+    is_priority: {
+      type: DataTypes.BOOLEAN,
+    },
   },
-  game: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  genre: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  difficulty: {
-    type: DataTypes.ENUM("easy" | "medium" | "hard"),
-    allowNull: true,
-  },
-  length: {
-    type: DataTypes.DECIMAL(2),
-    allowNull: true,
-  },
-  is_priority: {
-    type: DataTypes.BOOLEAN,
-  },
-  sequelize,
-  timestamps: false,
-  freezeTableName: true,
-  underscored: true,
-  modelName: "Game_List",
-	}
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "Game_List",
+  }
 );
 
-module.exports = List
+module.exports = Game_List
