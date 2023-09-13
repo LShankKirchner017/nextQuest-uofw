4c9f310f6f20849e7d033ea4e175044634a4347e
Akafifi <itsmeafifi@gmail.com>
2023-09-12 19:19:34 -0500

    test

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const gameRoutes = require('./gameRoutes')
 
 router.use('/users', userRoutes);
-router.use('/gamelists', gameRoutes)
+router.use('/gamelist', gameRoutes)
 
 module.exports = router;
