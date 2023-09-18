a59adf9e83b412052deef40267b6fa60d5eb5de3
Lindsay <lshank017@gmail.com>
2023-09-18 18:41:30 -0500

    gamelist now working

--- db/schema.sql
+++ db/schema.sql
@@ -1,25 +1,2 @@
 DROP DATABASE IF EXISTS gamer_db;
 CREATE DATABASE gamer_db;
-
-USE gamer_db;
-
--- CREATE TABLE user (
---     id INT NOT NULL AUTO_INCREMENT,
---     name VARCHAR(100) NOT NULL,
---     email VARCHAR(30) NOT NULL,
---     password VARCHAR(30) NOT NULL,
---     twitch VARCHAR(30),
---     PRIMARY KEY(id)
-
--- );
-
--- CREATE TABLE gamelist (
---     id INT NOT NULL AUTO_INCREMENT,
---     game VARCHAR(50) NOT NULL,
---     genre VARCHAR(30),
---     difficulty ENUM("easy", "medium", "hard")
---     length VARCHAR(30),
---     is_priority BOOLEAN DEFAULT 0,
---     date_created DATE 
-
--- )
